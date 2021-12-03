11c9f69efdc41de1d060de9971365c7023468471
Wonziu <leonwonziu@gmail.com>
2021-12-04 00:52:21 +0100

    Finished 1st day

---
+++ Day_1/main.py
@@ -0,0 +1,15 @@
+filename = "input.txt"
+
+def first(lines):
+    return sum(1 for i in range(len(lines)) if lines[i] > lines[i - 1])
+
+def second(lines):
+    sums = [sum(lines[i:i+3]) for i in range(len(lines) - 2)]
+    return sum(1 for i in range(len(sums)) if (sums[i] > sums[i - 1]))
+
+count = -1
+lastLine = -1
+if __name__ == '__main__':
+    with open(filename) as file:
+        data = [int(line.rstrip()) for line in file]
+    print(second(data))
