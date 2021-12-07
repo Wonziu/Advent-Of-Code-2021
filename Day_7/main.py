5e0e5ec210dedb6d0165aaf63c37a0383e270a01
Wonziu <leonwonziu@gmail.com>
2021-12-07 15:26:57 +0100

    Finished 7th day

---
+++ Day_7/main.py
@@ -0,0 +1,24 @@
+import statistics
+import math
+
+def first(data):
+    align = round(statistics.median(data))
+    return sum(abs(align - x) for x in data)
+
+def triangular_distance(x, y):
+    return int(abs(x - y) * (1 + abs(x - y)) / 2)
+
+def second(data):
+    align = statistics.mean(data)
+    lower = int(math.floor(align))
+    upper = int(math.ceil(align))
+    return min(sum(triangular_distance(lower, x) for x in data), sum(triangular_distance(upper, x) for x in data))
+
+filename = "input.txt"
+
+if __name__ == '__main__':
+    with open(filename) as file:
+        data = list(map(int, file.read().rstrip().split(',')))
+        print(first(data))
+        print(second(data))
+        
