f057b080e7888ee4cbbf39a8d0f6855c5e22dc0b
Wonziu <leonwonziu@gmail.com>
2021-12-15 16:44:43 +0100

    Finished 15th day (unoptimized Dijkstra)

---
+++ Day_15/main.py
@@ -0,0 +1,40 @@
+import heapq
+import time
+
+filename = "/home/wonziu/Documents/adventofcode/Day_15/input.txt"
+
+def dijkstra(board, depth):
+    height = len(board)
+    width = len(board[0])
+    
+    queue = [(0, (0, 0))]
+    dist = {(0, 0): 0}
+
+    while queue:
+        _, node = heapq.heappop(queue)
+        distance = dist[(node[0], node[1])]
+
+        for dir in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            x, y = node
+            dx, dy = dir
+            new_x = x + dx
+            new_y = y + dy
+
+            if new_x >= 0 and new_x < width * depth and new_y >= 0 and new_y < height * depth:
+                neigh_distance = distance + (board[new_x % width][new_y % height] + 
+                    new_x // width + new_y // height - 1) % 9 + 1
+
+                if neigh_distance < dist.get((new_x, new_y), float('inf')):
+                    dist[(new_x, new_y)] = neigh_distance
+                    heapq.heappush(queue, (neigh_distance, (new_x, new_y)))
+
+    return dist[(height * depth - 1, width * depth - 1)]
+
+if __name__ == '__main__':
+    with open(filename) as file:
+        data = file.read().rstrip().split()
+        data = [list(map(int, list(line))) for line in data]
+        
+        start_time = time.time()
+        print(dijkstra(data, 5))
+        print("--- %s seconds ---" % (time.time() - start_time))
