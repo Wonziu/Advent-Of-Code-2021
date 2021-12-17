bd2872d574523aead3c83ce4c6e06e2fc0e4f934
Wonziu <leonwonziu@gmail.com>
2021-12-17 18:18:57 +0100

    Solved first part of 17th day

--- Day_17/main.py
+++ Day_17/main.py
@@ -3,30 +3,12 @@ import math
 filename = "/home/wonziu/Documents/adventofcode/Day_17/input.txt"
 
 def first(x_interval, y_interval):
-    x0, x1 = x_interval
-    y0, y1 = y_interval
-
-    ys = []
-    for y in range(20):
-        for step in range(100):
-            print("y:", y, " step:", step, " pos:", (2 * y - step + 1) * step / 2)
-
-        # discriminant_lower = (2 * y + 1) ** 2 - 8 * y0
-        # discriminant_upper = (2 * y + 1) ** 2 - 8 * y1
-        # if discriminant_lower < 0 or discriminant_upper < 0:
-        #     print(y, " brak")
-        #     continue
-
-        # # # numbers in between are included
-        # lower_bound = [math.ceil(((2 * y + 1) - math.sqrt(discriminant_lower)) / 2), math.floor(((2 * y + 1) + math.sqrt(discriminant_lower)) / 2)]
-
-        # # # numbers in between are excluded
-        # upper_bound = [math.floor(((2 * y + 1) - math.sqrt(discriminant_upper)) / 2), math.ceil(((2 * y + 1) + math.sqrt(discriminant_upper)) / 2)]
-
-        # left_interval = [lower_bound[0], upper_bound[0]]
-        # right_interval = [upper_bound[1], lower_bound[1]]
-        # ys.append((y, left_interval, right_interval))
-        
+    # Because we are always gonna land on point (x, 0) with velocity (_, -y - 1) so for
+    # y = abs(y0 + 1) we're always gonna overshoot
+    greatest_y = abs(y_interval[0] + 1)    
+    # highest is gonna be the value of arithmetic sum of a sequence y_n = y - n + 1 from 1 to y
+    highest_y = (greatest_y + 1) * greatest_y // 2
+    return highest_y
 
 
 if __name__ == '__main__':
